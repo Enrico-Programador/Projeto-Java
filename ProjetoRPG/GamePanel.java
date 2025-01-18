5759a5145d8c06188768880d9ec2f80fbf15a2c7
ENRICO MATIAZZO GARCIA <enrico2015ap@gmail.com>
2025-01-18 17:44:55 -0300

    Passos Iniciais
    Fazendo um jogo primeiros passos inseridos como movimento e, labels e frames.

--- ProjetoRPG/GamePanel.java
+++ ProjetoRPG/GamePanel.java
@@ -1,23 +1,33 @@
 import java.awt.Color;
 import java.awt.Dimension;
-import javax.swing.JPanel;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
 
 public class GamePanel extends JPanel implements Runnable{
 
-      final int originalTileSize = 32;
-      final int scale = 1;
+      final int originalTileSize = 16;
+      final int scale = 3;
       final int tileSize = originalTileSize * scale;
       final int maxScreenCol = 16;
       final int maxScreenRow = 12;
       final int screenWidth = tileSize * maxScreenCol;
       final int screenHeight = tileSize * maxScreenRow;
+
+      Keys keyH = new Keys();
       Thread gameThread;
+
+      //default position
+      int playerX = 100;
+      int playerY = 100;
+      int playerSpeed = 4;
 
       public GamePanel() {
       this.setPreferredSize(new Dimension(screenWidth, screenHeight));
       this.setBackground(Color.BLUE);
       this.setDoubleBuffered(true);
+      this.addKeyListener(keyH);
       }
 
       public void startGameThread(){
@@ -32,7 +42,11 @@ public class GamePanel extends JPanel implements Runnable{
 
      while (gameThread != null){
 
+      System.out.println("the game is running");
+      //atualizar as informações do jogo
       update();
+
+      //desenhar a tela com as informações atualizadas
       repaint();
 
      }
@@ -41,14 +55,35 @@ public class GamePanel extends JPanel implements Runnable{
 
   public void update(){
 
-
+    if(keyH.upPress == true){
+     playerY = playerY - playerSpeed;
+    }
+    else if(keyH.downPress == true){
+      playerY = playerX + playerSpeed;
+    }
+    else if(keyH.rightPress == true){
+      playerX = playerX - playerSpeed;
+    }
+    else if(keyH.leftPress == true){
+      playerX = playerX + playerSpeed;
+    }
 
   }
 
+  //paintComponent é como se fosse seu pincel/lápis
+      @Override
   public void paintComponent(Graphics g){
 
+//super é para puxar o jPanel sua "Parent Class" no caso
 super.paintComponent(g);
+Graphics2D g2 = (Graphics2D)g;
+g2.setColor(Color.white);
+g2.fillRect(playerX, playerY, tileSize, tileSize);
+          //x            y     width     height
 
+//salvar memória          
+g2.dispose();
   }
-     
+
+
 }
